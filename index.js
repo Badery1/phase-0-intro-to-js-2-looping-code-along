fcebc91165762c746431c91c05be64ac04e37c6d
barit <mmyers.com@Gmail.com>
2023-08-09 19:14:22 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let cardboys = [];
+    for (let i = 0; i < names.length; i++) {
+        cardboys.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        
+        
+    }
+    return cardboys;
+};
+function countDown(num) {
+   
+   if (num != 4) {
+    while (num >= 0) {
+            console.log()    
+            num--;
+            
+        }
+    }      
+    if(num === 4) {
+        while (num >= 0) {
+            console.log(num)
+            num--
+        }
+    }
+}
